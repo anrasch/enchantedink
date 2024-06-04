416a7a864ca697865adcf621ed5ab98792dbe04c
anrasch <anthony.rasch@googlemail.com>
2024-06-04 08:29:13 +0200

    version0.5

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,13 @@ const translations = {
         title: "Welcome to EnchantedInk",
         subtitle: "Author of Fantasy Novels and Short Stories",
         navAbout: "About",
-        navBooks: "Books/Projects",
+        navBooks: "Books",
+        navProjects: "Projects",
         navBlog: "Blog",
         navContact: "Contact",
         aboutText: "Your biography and background information.",
         booksText: "Detailed descriptions of your books or short stories.",
+        projectsText: "Detailed descriptions of your projects.",
         blogText: "Regular posts about your writing processes, inspirations, and news.",
         contactText: "Contact information and a contact form."
     },
@@ -15,11 +17,13 @@ const translations = {
         title: "Willkommen bei EnchantedInk",
         subtitle: "Autor von Fantasy-Romanen und Kurzgeschichten",
         navAbout: "Über mich",
-        navBooks: "Bücher/Projekte",
+        navBooks: "Bücher",
+        navProjects: "Projekte",
         navBlog: "Blog",
         navContact: "Kontakt",
         aboutText: "Ihre Biografie und Hintergrundinformationen.",
         booksText: "Detaillierte Beschreibungen Ihrer Bücher oder Kurzgeschichten.",
+        projectsText: "Detaillierte Beschreibungen Ihrer Projekte.",
         blogText: "Regelmäßige Beiträge über Ihre Schreibprozesse, Inspirationen und Neuigkeiten.",
         contactText: "Kontaktinformationen und ein Kontaktformular."
     }
@@ -37,10 +41,12 @@ function setLanguage(language) {
     document.getElementById('subtitle').innerText = translations[language].subtitle;
     document.getElementById('nav-about').innerText = translations[language].navAbout;
     document.getElementById('nav-books').innerText = translations[language].navBooks;
+    document.getElementById('nav-projects').innerText = translations[language].navProjects;
     document.getElementById('nav-blog').innerText = translations[language].navBlog;
     document.getElementById('nav-contact').innerText = translations[language].navContact;
     document.getElementById('about-text').innerText = translations[language].aboutText;
     document.getElementById('books-text').innerText = translations[language].booksText;
+    document.getElementById('projects-text').innerText = translations[language].projectsText;
     document.getElementById('blog-text').innerText = translations[language].blogText;
     document.getElementById('contact-text').innerText = translations[language].contactText;
 
